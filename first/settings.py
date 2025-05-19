13d1f3371bb3e9ea7da27078d1c870df68064760
Asrorbek Aliqulov <asrorbek@asror.uz>
2025-05-19 21:05:37 +0500

    hello5

--- first/settings.py
+++ first/settings.py
@@ -78,5 +78,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICSSTORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
